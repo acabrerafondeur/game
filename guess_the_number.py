22c6e6b9e1c93ceb9c037b3770d9ea34c0b201ce
AM <47997094+acabrerafondeur@users.noreply.github.com>
2023-07-30 21:09:14 -0600

    Create guess_the_number.py

---
+++ guess_the_number.py
@@ -0,0 +1,27 @@
+import random
+
+def guess_the_number():
+    print("Welcome to the Guess the Number game!")
+    print("I'm thinking of a number between 1 and 100.")
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Take a guess: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low. Try again.")
+        elif guess > secret_number:
+            print("Too high. Try again.")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts!")
+            break
+
+if __name__ == "__main__":
+    guess_the_number()
